1593602b326a70023918d86cdce3828af796bff8
guarenty <jonasschirrmeister@gmx.de>
2023-11-01 18:07:49 +0100

    first commit

---
+++ nuxt-app/app.vue
@@ -0,0 +1,12 @@
+<template>
+  {{ data }}
+</template>
+
+<script setup lang="ts">
+const supabase = useSupabaseClient()
+
+const { data, error } = await supabase.auth.signInWithPassword({
+  email: "tester@schirri.de",
+  password: "password"
+})
+</script>
